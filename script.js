b7c047bd70c9b604d4175aa7c73ac0f588ea351b
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2023-05-27 22:05:56 -0300

    fromulário "funcional"

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ statusTxt = form.querySelector(".button-area span")
 
 form.onsubmit = (e) =>{
     e.preventDefault() //Preveni para que o formuláririo não envie
+    statusTxt.style.color = "#0D6EFD"
     statusTxt.style.display = "block"
 
     let xhr = new XMLHttpRequest()
@@ -12,7 +13,15 @@ form.onsubmit = (e) =>{
     xhr.onload = () => {// quando o ajax estiver carregado
         if(xhr.readyState == 4 && xhr.status == 200){//Se o status do ajax for 4 significa que têm nenhum erro
             let response = xhr.response // gaurda a resposta do ajax em uma variável resposta  
-            console.log(response)
+            //Se a resposta for um erro, troca aa menssagem em baixo para vermelho senão reinicia o formulário 
+            if(response.indexOf("O email e a mensagem são campos obrigatórios") != -1 || response.indexOf("Seu email precisa ser válido para prosseguir") || response.indexOf("Desculpe, falha ao enviar a meenssagem!")){
+                statusTxt.style.color = "red"
+            }else{
+                form.reset()
+                setTimeout(()=>{
+                    statusTxt.style.display= "none"
+                }, 3000)
+            }
             statusTxt.innerText = response
         }
 
